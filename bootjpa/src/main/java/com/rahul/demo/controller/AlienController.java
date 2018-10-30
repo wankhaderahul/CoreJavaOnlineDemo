32b3fd85ea002bb0df4a12ae6a4471268a63ab3c
FWIN01617 <FWIN01617@FLPUNPDSD846.fww.com>
2018-10-30 20:12:45 +0530

    all commited

--- bootjpa/src/main/java/com/rahul/demo/controller/AlienController.java
+++ bootjpa/src/main/java/com/rahul/demo/controller/AlienController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,12 +13,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rahul.demo.model.Alien;
 import com.rahul.demo.repo.AlienRepo;
+
+
 
 //@Controller
 @RestController
